41f9acb4d7b365a83a8656c60e9fc1debee4fc90
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-25 17:50:37 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+var text = "Sam is working at codingschool";
+
+
+console.log(text.substr(0,3))
+
+let sam =text.substr(0,3);
+
+console.log(text.substr(3,4))
+
+let is =text.substr(3,4);
+
+console.log(text.substr(15,2))
+
+console.log(text.substr(24,6,))
+
+let school =text.substr(24,6,);
+
+let at =text.substr(15,2);
+
+console.log(text.substr(7,4))
+
+let work=text.substr(7,4)+"s";
+
+
+
+
+
+
+
+
+
+
+
+document.write("<h1>" + sam + "</h1>"+"</br>")
+
+document.write("<h1>" + is + "</h1>"+"</br>")
+
+document.write("<h1>" +at+" "+ school + "</h1>"+"</br>")
+
+document.write("<h1>" + sam +" "+work+" "+at+" "+school+ "</h1>"+"</br>")
